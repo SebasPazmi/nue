f8a0feb27012ff487e7469b6d13b8d1158f17062
SEBASTIANPAZMINO <sebastian123001@outlook.es>
2023-08-02 10:50:05 -0500

    commttttttttti33333333333333

--- new/js.js
+++ new/js.js
@@ -5,3 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
         alert('¡Has hecho clic en el botón!');
     });
 });
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById('myButton');
+    var paragraph = document.getElementById('demo');
+
+    button.addEventListener('click', function() {
+        paragraph.innerHTML = '¡El texto ha cambiado!';
+    });
+});
+
